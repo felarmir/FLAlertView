7e54107284f6e28e55d48253e4a2184fcbe921bd
Denis Andreev <felarmir@gmail.com>
2024-08-24 15:36:26 +0300

    add package module

--- Package.swift
+++ Package.swift
@@ -1,13 +1,9 @@
 // swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "FLAlertView",
-    platforms: [
-        .iOS(.v14)
-    ],
     products: [
         .library(
             name: "FLAlertView",
